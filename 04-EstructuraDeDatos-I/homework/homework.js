fa3e2c08e56a605e202d09fe2a3d7f525ccbf401
Seba <sebastian.pitra10@gmail.com>
2022-09-15 14:36:49 -0300

    nFibonacci finished

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -15,18 +15,29 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
-   //Formula de factoria --> 5 * 4! 
+   //Formula de factorial --> 5 * 4! 
    if(n === 0 || n === 1) return 1
+   else if(n < 0) return 0
    return n * nFactorial(n - 1)
 }
-// console.log(nFactorial())
+console.log(nFactorial(5))
 
-// nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
-// Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
-// Secuencia:  0, 1, 1, 2, 3, 5  , 8, 13, 21, 34, ... 
 function nFibonacci(n) {
+   //Formula de fibonacci: fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+   //caso base: Cuando llegue a 0 o a 1;
+   if (n == 0) return 0
+   else if(n == 1) return 1
+   else return nFibonacci(n-1) + nFibonacci(n-2)
 }
+console.log(nFibonacci(7))
+
+// nFibonacci(7) = nFibonacci(6) + nFibonacci(5)
+// nFibonacci(6) = nFibonacci(5) + nFibonacci(4)
+// nFibonacci(5) = nFibonacci(4) + nFibonacci(3)
+// nFibonacci(4) = nFibonacci(3) + nFibonacci(2)
+// nFibonacci(3) = nFibonacci(2) + nFibonacci(1)
+// nFibonacci(2) = nFibonacci(1) + nFibonacci(0)  
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
